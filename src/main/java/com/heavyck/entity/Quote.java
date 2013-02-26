47ed1f3ac76e652b6e3ad9bb95837fa7317c8b1c
Admin <Admin@INFINITI>
2013-02-26 22:05:39 +0200

    Initial commit

---
+++ src/main/java/com/heavyck/entity/Quote.java
@@ -0,0 +1,54 @@
+package com.heavyck.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "quotes")
+public class Quote {
+
+    @Id
+    @Column
+    Long id;
+
+    @Column(name = "quote")
+    String quote;
+
+    @Column(name = "category")
+    String category;
+
+    @ManyToOne
+    @JoinColumn(name = "author")
+    Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/quotes.jsp
@@ -0,0 +1,14 @@
+<%--
+  User: Администратор
+  Date: 26.02.13
+  Time: 21:41
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Quotes</title>
+    </head>
+    <body>
+        To be developed...
+    </body>
+</html>
